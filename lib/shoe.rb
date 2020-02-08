cb72a76755f572fdbfa744831ad24c8b4bb85894
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-08 21:45:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,15 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def cobble
+    @condition = "new"
+  end
 end
